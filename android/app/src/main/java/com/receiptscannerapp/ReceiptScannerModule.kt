2fc7bbd2c49be82cca48bf55f9c2eb191f895169
Choi Eun Jeong <eunjeongchoi0805@gmail.com>
2026-08-27 23:05:05 +0900

    feat: scanText 네이티브 모듈 구현 (ML Kit Text Recognition 연동)

--- android/app/src/main/java/com/receiptscannerapp/ReceiptScannerModule.kt
+++ android/app/src/main/java/com/receiptscannerapp/ReceiptScannerModule.kt
@@ -1,11 +1,33 @@
 package com.receiptscannerapp
 
+import android.net.Uri
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions
 
 class ReceiptScannerModule(reactContext: ReactApplicationContext) :
   NativeReceiptScannerSpec(reactContext) {
 
   override fun multiply(a: Double, b: Double): Double {
     return a * b
+  }
+
+  override fun scanText(imageUri: String, promise: Promise) {
+    try {
+      val recognizer = TextRecognition.getClient(KoreanTextRecognizerOptions.Builder().build())
+      val image = InputImage.fromFilePath(reactApplicationContext, Uri.parse(imageUri))
+
+      recognizer.process(image)
+        .addOnSuccessListener { visionText ->
+          promise.resolve(visionText.text)
+        }
+        .addOnFailureListener { e ->
+          promise.reject("SCAN_ERROR", e)
+        }
+    } catch (e: Exception) {
+      promise.reject("SCAN_ERROR", e)
+    }
   }
 }
